dbe0dff223c602287f7d6a93d6d35c38394dfea3
Shreyyyas22 <waghshreyash22@gmail.com>
2023-08-01 13:30:06 +0530

    Updated Toastify

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
 import { Rating } from "../components/Elements/Rating";
+import { toast } from "react-toastify";
 import { useTitle } from "../hooks/useTitle";
 import { useParams } from "react-router-dom";
 import { useCart } from "../context";
 import { getProduct } from "../services";
-import { toast } from "react-toastify";
 
 export const ProductDetail = () => {
   const { cartList, addToCart, removeFromCart } = useCart();
