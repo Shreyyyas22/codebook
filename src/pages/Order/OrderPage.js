cb8ca1d7cac0521ceb2e2397e006f0965173bdb0
Shreyyyas22 <waghshreyash22@gmail.com>
2023-08-01 11:41:17 +0530

    CodeBook Deployment

---
+++ src/pages/Order/OrderPage.js
@@ -0,0 +1,14 @@
+import {useTitle} from "../../hooks/useTitle";
+import { OrderSuccess } from "./components/OrderSuccuss";
+import { OrderFail } from "./components/OrderFail";
+import { useLocation } from "react-router-dom";
+
+export const OrderPage = () => {
+  useTitle("Order Summary")
+    const {state} = useLocation();
+    return (
+        <main>
+          { state.status ? <OrderSuccess data={state.data} /> : <OrderFail /> }
+        </main>
+      )
+    }
